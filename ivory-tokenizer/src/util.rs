f6840b521d82f8812203b4a1ebac23c13d5f90f0
mcpar-land <john@mcpar.land>
2021-11-05 23:28:15 -0500

    minor fix to imports

--- ivory-tokenizer/src/util.rs
+++ ivory-tokenizer/src/util.rs
@@ -4,11 +4,11 @@ use nom::{
 	branch::alt,
 	bytes::complete::tag,
 	character::complete::{
-		alpha1, alphanumeric1, multispace0, multispace1, one_of, space0,
+		alpha1, alphanumeric1, multispace0, multispace1, one_of,
 	},
 	combinator::recognize,
 	multi::{many0, many1},
-	sequence::{pair, separated_pair, tuple},
+	sequence::{pair, tuple},
 	IResult,
 };
 
@@ -84,72 +84,78 @@ pub fn comma_separated_display<T: Display>(vec: &Vec<T>) -> String {
 }
 
 #[cfg(test)]
-#[test]
-fn test_snake_case() {
-	assert!(snake_case("this_is_a_snake_case_word").is_ok());
-	assert!(snake_case("ThisIsNotSnakeCase").is_err());
-	assert!(snake_case("_this_is_snake_case").is_ok());
-}
+mod test {
+	use nom::sequence::separated_pair;
 
-#[test]
-fn test_variable_name() {
-	let variables = [
-		"this_is_a_variable_name",
-		"ThisIsAVariableName",
-		"thisisavariablename123",
-	];
-	let bad_variables = ["69badname", " ooooo"];
-	for v in &variables {
-		assert!(variable_name(v).is_ok());
+	use super::*;
+
+	#[test]
+	fn test_snake_case() {
+		assert!(snake_case("this_is_a_snake_case_word").is_ok());
+		assert!(snake_case("ThisIsNotSnakeCase").is_err());
+		assert!(snake_case("_this_is_snake_case").is_ok());
 	}
-	for v in &bad_variables {
-		assert!(variable_name(v).is_err());
+
+	#[test]
+	fn test_variable_name() {
+		let variables = [
+			"this_is_a_variable_name",
+			"ThisIsAVariableName",
+			"thisisavariablename123",
+		];
+		let bad_variables = ["69badname", " ooooo"];
+		for v in &variables {
+			assert!(variable_name(v).is_ok());
+		}
+		for v in &bad_variables {
+			assert!(variable_name(v).is_err());
+		}
 	}
-}
 
-#[test]
-fn test_comment_whitespace0() {
-	let mut test = separated_pair(tag("foo"), ws0, tag("bar"));
-	assert_eq!(test("foobar").unwrap().1, ("foo", "bar"));
-	assert_eq!(test("foo bar").unwrap().1, ("foo", "bar"));
-	assert_eq!(
-		test("foo # this is a comment\nbar").unwrap().1,
-		("foo", "bar")
-	);
-	assert_eq!(
-		test("foo \n # this is a comment\n\n\n# this is a comment too \n bar")
-			.unwrap()
-			.1,
-		("foo", "bar")
-	);
-	assert!(test("foo # this is a comment bar").is_err());
-}
+	#[test]
+	fn test_comment_whitespace0() {
+		let mut test = separated_pair(tag("foo"), ws0, tag("bar"));
+		assert_eq!(test("foobar").unwrap().1, ("foo", "bar"));
+		assert_eq!(test("foo bar").unwrap().1, ("foo", "bar"));
+		assert_eq!(
+			test("foo # this is a comment\nbar").unwrap().1,
+			("foo", "bar")
+		);
+		assert_eq!(
+			test("foo \n # this is a comment\n\n\n# this is a comment too \n bar")
+				.unwrap()
+				.1,
+			("foo", "bar")
+		);
+		assert!(test("foo # this is a comment bar").is_err());
+	}
 
-#[test]
-fn test_comment_whitespace1() {
-	let mut test = separated_pair(tag("foo"), ws1, tag("bar"));
-	assert_eq!(test("foo bar").unwrap().1, ("foo", "bar"));
-	assert_eq!(
-		test("foo #this is a comment\nbar").unwrap().1,
-		("foo", "bar")
-	);
-	assert_eq!(
-		test("foo#this is a comment\nbar").unwrap().1,
-		("foo", "bar")
-	);
-	assert_eq!(
-		test("foo#this is a comment\n bar").unwrap().1,
-		("foo", "bar")
-	);
-	assert_eq!(
-		test("foo #this is a comment\n bar").unwrap().1,
-		("foo", "bar")
-	);
-	assert_eq!(
-		test("foo # this is a comment\n#also a comment\nbar")
-			.unwrap()
-			.1,
-		("foo", "bar")
-	);
-	assert!(test("foobar").is_err());
+	#[test]
+	fn test_comment_whitespace1() {
+		let mut test = separated_pair(tag("foo"), ws1, tag("bar"));
+		assert_eq!(test("foo bar").unwrap().1, ("foo", "bar"));
+		assert_eq!(
+			test("foo #this is a comment\nbar").unwrap().1,
+			("foo", "bar")
+		);
+		assert_eq!(
+			test("foo#this is a comment\nbar").unwrap().1,
+			("foo", "bar")
+		);
+		assert_eq!(
+			test("foo#this is a comment\n bar").unwrap().1,
+			("foo", "bar")
+		);
+		assert_eq!(
+			test("foo #this is a comment\n bar").unwrap().1,
+			("foo", "bar")
+		);
+		assert_eq!(
+			test("foo # this is a comment\n#also a comment\nbar")
+				.unwrap()
+				.1,
+			("foo", "bar")
+		);
+		assert!(test("foobar").is_err());
+	}
 }
